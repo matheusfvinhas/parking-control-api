6b3f1160e0e388d917c852140d4e30ea007cd0b6
Matheus Silva <matheusfvinhas@gmail.com>
2019-11-13 00:02:10 -0300

    chenge to sqlite

--- INSTALL.md
+++ INSTALL.md
@@ -1,3 +1,3 @@
 ```sh
-    bundle && rails s
+    bundle && rails db:create && rails db:migrate && rails db:migrate RAILS_ENV=test && rails s
 ```
--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_11_06_172348) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "parkings", force: :cascade do |t|
     t.string "plate", null: false
     t.boolean "paid", default: false, null: false
